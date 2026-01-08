18fc5dc83b033e32034f740fb814816c855538b7
Lieberty Holanda <lieberty.dev@gmail.com>
2026-01-08 19:37:04 -0300

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,26 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path'); // CommonJS
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'), // pasta única para deploy
-    filename: 'bundle.js',
-    clean: true // limpa o dist antes de build
-  },
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: { presets: ['@babel/env'] }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html', // pega seu HTML de template
-      filename: 'index.html'
-    })
-  ],
-  devtool: 'source-map'
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                presets: ['@babel/env']
+               } 
+            }
+        }, {
+           test: /\.css$/,
+           use: ['style-loader', 'css-loader'] 
+        }]
+    },
+    devtool: 'source-map' //mapeia os erros no arq original
 };
